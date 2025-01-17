ebf81519917cd893c7d9fef7fd9cef6ff07e8a75
s.jorayev <s.jorayev@newuu.uz>
2025-01-17 12:24:59 +0500

    My first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cout << "Hello, how old are you " << endl;
+    cin >> age;
+    cout << "18:"<<age<<endl;
+    return 0;
+}
+
